4171529f4b974fd4e38677687367bb802af395a5
T.E.A de Souza <tea.desouza@gmail.com>
2025-06-10 14:19:05 +0800

    Support api key from editor prefs

--- Runtime/Scripts/EdenAI-API.cs
+++ Runtime/Scripts/EdenAI-API.cs
@@ -3,16 +3,16 @@ using System.IO; using System.Net.Http;
 using System.Threading.Tasks; using System.Text;
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEditor;
 
-namespace EdenAI
-{
+namespace EdenAI{
+
     [Serializable]
     public class Key{
         public string api_key { get; set; }
     }
 
-    public partial class EdenAIApi
-    {
+    public partial class EdenAIApi{
         //static bool log_queries = true;
         private string _apiKey;
         private static readonly HttpClient _httpClient = new HttpClient();
@@ -24,31 +24,32 @@ namespace EdenAI
         ){
             this.log = log; this.logw = logw; this.log_err = log_err;
             _httpClient.Timeout = TimeSpan.FromSeconds(200);
-            if (!string.IsNullOrEmpty(apiKey))
-            {
+            if (!string.IsNullOrEmpty(apiKey)){
                 this._apiKey = apiKey;
-            }
-            else
-            {
-                var userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                var authPath = $"{userPath}/.edenai/auth.json";
-                if (File.Exists(authPath))
+            }else{
+                // Try to load from Unity Editor Preferences
+                this._apiKey = EditorPrefs.GetString("EdenAI_API_Key", "");
+
+                if (string.IsNullOrEmpty(this._apiKey))
                 {
-                    var json = File.ReadAllText(authPath);
-                    Key auth = JsonConvert.DeserializeObject<Key>(json);
-                    this._apiKey = auth.api_key;
-                }
-                else
-                {
-                    throw new Exception("API Key is null and auth.json does not exist.");
+                    var userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    var authPath = $"{userPath}/.edenai/auth.json";
+                    if (File.Exists(authPath))
+                    {
+                        var json = File.ReadAllText(authPath);
+                        Key auth = JsonConvert.DeserializeObject<Key>(json);
+                        this._apiKey = auth.api_key;
+                    }
+                    else
+                    {
+                        throw new Exception("API Key is null and auth.json does not exist. Please set the API Key in the Unity Editor Preferences (EdenAI_API_Key) or create an auth.json file.");
+                    }
                 }
             }
         }
 
-        private void AddHeaders(HttpRequestMessage request)
-        {
-            if (string.IsNullOrEmpty(this._apiKey))
-            {
+        private void AddHeaders(HttpRequestMessage request){
+            if (string.IsNullOrEmpty(this._apiKey)){
                 throw new Exception("Missing API Key");
             }
             request.Headers.Add("Authorization", "Bearer " + this._apiKey);
@@ -67,15 +68,19 @@ namespace EdenAI
 
             HttpContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
             request.Content = content;
-            log($"CONTENT\n{jsonPayload}");
+            string prettyJson = JsonConvert.SerializeObject(payload, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            log($"CONTENT\n{prettyJson}");
+
             HttpResponseMessage response = await _httpClient.SendAsync(request);
             string responseText = await response.Content.ReadAsStringAsync();
             if (!response.IsSuccessStatusCode){
                 Debug.LogError(responseText);
                 response.EnsureSuccessStatusCode();
             }
-            log($"RESPONSE\n{responseText}");
+            var formattedJson = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseText), Formatting.Indented);
+            log($"RESPONSE\n{formattedJson}");
             return responseText;
+
         }
 
     }
